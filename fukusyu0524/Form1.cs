3ab7691dd3e4a5193176a6ae80af93a43dbd1c86
kai-machida <49702001+kai-machida@users.noreply.github.com>
2019-05-24 13:28:44 +0900

    未完成

--- fukusyu0524/Form1.cs
+++ fukusyu0524/Form1.cs
@@ -12,9 +12,37 @@ namespace fukusyu0524
 {
     public partial class Form1 : Form
     {
+        int vx = 0;
+        int vy = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
